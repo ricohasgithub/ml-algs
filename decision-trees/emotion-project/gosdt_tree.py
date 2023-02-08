41ddeece74c47ced386b26a3fd9ea912456f2b04
Rico Zhu <rico.zhu@duke.edu>
2023-02-08 14:06:12 -0500

    Completed Chord Class

--- decision-trees/emotion-project/gosdt_tree.py
+++ decision-trees/emotion-project/gosdt_tree.py
@@ -3,19 +3,20 @@ import numpy as np
 import time
 import pathlib
 
-from sklearn.ensemble import GradientBoostingClassifier
-from model.threshold_guess import compute_thresholds
-from model.gosdt import GOSDT
+ROMANS = ["I", "ii", "iii", "IV", "V", "VI", "vii"]
 
 EMOTIONS = ["NONE", "ANGER", "SAD", "FEAR", "IRONY", "LOVE", "JOY"]
 EMOTIONS_MAP = {"NONE": 0, "ANGER": 1, "SAD": 2, "FEAR": 3, "IRONY": 4, "LOVE": 5, "JOY": 6}
-ROMANS = ["I", "ii", "iii", "IV", "V", "VI", "vii"]
+
 MAJOR_WEIGHTS = [0.04, 0.06, 0.01, 0.02, 0.12, 0.27, 0.48]
 MINOR_WEIGHTS = [0.01, 0.31, 0.33, 0.25, 0.05, 0.03, 0.02]
 
 def get_distribution(X, total):
     # Given some list x with discrete samples, return percentage distribution
-    return [x / total for x in X]
+    sum_X = [0, 0, 0, 0, 0, 0, 0]
+    for x in X:
+        sum_X[EMOTIONS_MAP[x]] += 1
+    return [s_x / total for s_x in sum_X]
 
 class Chord():
 
@@ -23,14 +24,14 @@ class Chord():
         self.roman = roman
         if roman.isupper():
             # Major emotional distribution; greater weight on LOVE, JOY
-            self.discrete_dist = np.random.choice(EMOTIONS, 100, MAJOR_WEIGHTS)
-            self.emotion_dist = get_distribution(self.discrete_dist)
-            print(self.emotion_dist)
+            self.discrete_dist = np.random.choice(EMOTIONS, 100, p=MAJOR_WEIGHTS)
+            self.emotion_dist = get_distribution(self.discrete_dist, 100)
         else:
             # Minor emotional distribution; greater weight on ANGER, SAD, FEAR, IRONY
-            self.discrete_dist = np.random.choice(EMOTIONS, 100, MINOR_WEIGHTS)
-            self.emotion_dist = get_distribution(self.discrete_dist)
-            print(self.emotion_dist)
+            self.discrete_dist = np.random.choice(EMOTIONS, 100, p=MINOR_WEIGHTS)
+            self.emotion_dist = get_distribution(self.discrete_dist, 100)
+
+CHORDS = [Chord(r) for r in ROMANS]
 
 class Progression():
 
