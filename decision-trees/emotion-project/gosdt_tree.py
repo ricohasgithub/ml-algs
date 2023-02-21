6a437c7abda5a4e4cbabde9d56e5365b4eace109
Rico Zhu <rico.zhu@duke.edu>
2023-02-21 13:03:15 -0500

    Added sklearn decision tree

--- decision-trees/emotion-project/gosdt_tree.py
+++ decision-trees/emotion-project/gosdt_tree.py
@@ -4,3 +4,14 @@ import numpy as np
 
 from music_utils import generate_musical_data_as_pd
 from sklearn import tree
+
+def sklearn_tree(N):
+
+    # Get training data
+    X, Y = generate_musical_data_as_pd(N)
+
+    # Init tree and fit
+    decision_tree = tree.DecisionTreeClassifier()
+    decision_tree.fit(X, Y)
+
+    
