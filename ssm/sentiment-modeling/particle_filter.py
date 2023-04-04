d84f324cb5ce528ea0baa831789d2d512445f348
Rico Zhu <rico.zhu@duke.edu>
2023-04-03 22:00:47 -0400

    Added noramlizing function

--- ssm/sentiment-modeling/particle_filter.py
+++ ssm/sentiment-modeling/particle_filter.py
@@ -4,10 +4,11 @@ import numpy as np
 import filterpy
 import matplotlib.pyplot as plt
 
+from numpy.linalg import norm
 from numpy.random import randn, dirichlet
 
 def normalize(v):
-    pass
+    return v / norm(v, ord=1)
 
 def visualize_emissions_matrix(matrix, numUnique, ItoV, ItoE, emotions):
     plt.imshow(matrix, cmap='hot', interpolation='nearest')
@@ -15,11 +16,14 @@ def visualize_emissions_matrix(matrix, numUnique, ItoV, ItoE, emotions):
     plt.yticks(range(len(emotions)), [ItoE[str(i)] for i in range(len(emotions))])
     plt.show()
 
+# Gets p(RN_t | p_t), where p_t is a mixture of emotions
 def get_RN_mixture_from_emotions(emotions, p_t):
     # Take a linear combination of the rows of the emotion matrix, weighted by the presence of each emotion in p_t
     emotions_matrix = np.asmatrix(emotions)
     mixture = p_t * emotions_matrix
-    return mixture.tolist()[0]
+    # Normalize
+    mixture = normalize(mixture.tolist()[0])
+    return mixture
 
 # Taken from: https://github.com/rlabbe/Kalman-and-Bayesian-Filters-in-Python/blob/master/12-Particle-Filters.ipynb
 def create_gaussian_particles(mean, std, N):
@@ -37,7 +41,6 @@ def main():
     emissions_matrix = np.load("emission_matrices.npy", allow_pickle=True)
     emissions_matrix_header = json.load(open("emission_matrices.json"))
     visualize_emissions_matrix(emissions_matrix, emissions_matrix_header[0], emissions_matrix_header[1], emissions_matrix_header[2], emissions_matrix_header[3])
-    print(get_RN_mixture_from_emotions(emissions_matrix, [0.5, 0.3, 0.1, 0.1, 0, 0, 0]))
 
 if __name__ == "__main__":
     main()
